3b9a44643a7189c19e3790a5c925b283713fd185
arkana-dev <developer@arkana.ai>
2018-09-07 02:52:18 +0700

    Brew formula update for arkana version v0.0.4

--- Formula/arkana.rb
+++ Formula/arkana.rb
@@ -1,9 +1,9 @@
 class Arkana < Formula
   desc "Command line tools for arkana systems"
   homepage "https://arkana.ai"
-  url "https://github.com/arkana-ai/cli/releases/download/v0.0.2/arkana_0.0.2_Darwin_x86_64.tar.gz"
-  version "0.0.2"
-  sha256 "7962b41e2035f1f5b3007bd70076f602fc31406b3b43f2161b6c9817d00dcc23"
+  url "https://github.com/arkana-ai/cli/releases/download/v0.0.4/arkana_0.0.4_Darwin_x86_64.tar.gz"
+  version "0.0.4"
+  sha256 "a49bdd55622eb33d3017f2c0f199a81e2cb40dbd4a8afcfb1c1c3213621625f0"
 
   def install
     bin.install "arkana"
